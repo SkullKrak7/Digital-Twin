5619ccdb1a794df627c99329d5d380ded3e63d23
SkullKrak7 <68808429+SkullKrak7@users.noreply.github.com>
2025-03-27 19:47:32 +0000

    improve data_gen to generate a csv file

--- init_db.py
+++ init_db.py
@@ -4,14 +4,16 @@ def init_db():
     """Creates a database table for storing sensor data"""
     conn = sqlite3.connect("sensor_data.db")
     cursor = conn.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS sensor (
-                    id INTEGER PRIMARY KEY,
+    cursor.execute('''CREATE TABLE IF NOT EXISTS sensors(
+                    id INTEGER KEY AUTOINCREMENT,
                     timestamp TEXT,
                     temperature REAL,
                     vibration REAL,
                     pressure REAL)''')
     conn.commit()
     conn.close()
+    print("Database initialized successfully!!!")
+
 
 if __name__ == "__main__":
     init_db()
